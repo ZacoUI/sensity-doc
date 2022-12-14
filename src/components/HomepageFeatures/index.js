b4029ebe37c9c22eb3fb2bc1a4edbcddb4d6c737
Nicolas <nicolas.francois360@gmail.com>
2022-12-14 14:07:12 +0100

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Site avec le dark theme ',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+    Un thème Dark est disponible pour éviter que tu te piques les yeux ^^ 
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Sensi Tuto',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+    Découvrez l'ensemble des tutoriels pour avoir une modération correcte sur Sensity 
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'En constante évolution ',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Découvre au fil du temps un ensemble d'outils qui seront disponibles.
       </>
     ),
   },
